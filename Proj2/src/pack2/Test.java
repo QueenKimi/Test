9c867720faa9f1780502a65fbeab482327cb05e4
kim <kim@10.250.233.70>
2022-10-01 20:07:24 -0400

    Test

---
+++ Proj2/src/pack2/Test.java
@@ -0,0 +1,10 @@
+package pack2;
+
+public class Test {
+			  public static void main(String[] args) {
+			    int x = 5;
+			    int y = 2;
+			    System.out.println(x % y + x * x);
+			  }			  
+			}
+
